4b5a3a39e6a460f8526433a2c183b3d3bde471f7
Yaron Sole <sole@nextamount-lm.corp.yahoo.com>
2014-06-15 14:23:39 -0700

    Initial Commit

---
+++ Week2HW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Week2HW
+//
+//  Created by Yaron Sole on 6/15/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Week2HWTests/Week2HWTests.m
@@ -0,0 +1,34 @@
+//
+//  Week2HWTests.m
+//  Week2HWTests
+//
+//  Created by Yaron Sole on 6/15/14.
+//  Copyright (c) 2014 codepath. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Week2HWTests : XCTestCase
+
+@end
+
+@implementation Week2HWTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
